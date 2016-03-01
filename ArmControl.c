3e767570acf5b61796e774679f39c89f1ffcf2b8
HCastano <castano.ha@gmail.com>
2016-02-29 20:11:41 -0500

    Add function bodies to liftElbow and rotateWrist

--- ArmControl.c
+++ ArmControl.c
@@ -1,11 +1,29 @@
 /*
+This file contains the code that will control the elbow joint, as
+well as the wrist code. 
+*/
+
+/*
 Activates the two motors at the elbow joint 
 in order to lift the arm. 
 
 Param lift Boolean containing whether to raise or drop the elbow
 */
 void liftElbow(bool lift){
+	
+	time[0] = 0; 
 
+	if (lift)
+		motor[motorA] = 75; 
+		motor[motorB] = 75; 
+	else
+		motor[motorA] = -75; 
+		motor[motorB] = -75; 
+	
+
+	while (time[0] < 2000){}
+	motor[motorA] = 0; 
+	motor[motorB] = 0; 
 }
 
 /*
@@ -15,7 +33,16 @@ activation of motors for a specific amount of time.
 Param cw Boolean contains the direction of rotation
 
 */
-void rotateWrist(bool cw){
+void rotateWrist(bool clockwise){
+	time[1] = 0; 
+
+	if (clockwise)
+		motor[motorC] = 35; 
+	else
+		motor[motorC] = -35; 
+
+	while (time[1] < 1000){}
+	motor[motorC] = 0; 
 
 }
 
