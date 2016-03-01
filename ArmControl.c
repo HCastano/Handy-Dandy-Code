ab58216a8d2268f606b2b6cfcee502f1e442894f
HCastano <castano.ha@gmail.com>
2016-02-29 19:16:41 -0500

    Add file for arm control (ArmControl.c)

---
+++ ArmControl.c
@@ -0,0 +1,24 @@
+/*
+Activates the two motors at the elbow joint 
+in order to lift the arm. 
+
+Param lift Boolean containing whether to raise or drop the elbow
+*/
+void liftElbow(bool lift){
+
+}
+
+/*
+Rotates the wrist in a given direction through the 
+activation of motors for a specific amount of time. 
+
+Param cw Boolean contains the direction of rotation
+
+*/
+void rotateWrist(bool cw){
+
+}
+
+task main(){
+	
+}
