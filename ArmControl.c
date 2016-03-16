701df72dd1de6f7b006de10bf99ad177cef09978
HCastano <castano.ha@gmail.com>
2016-03-16 05:14:16 -0400

    Add demo functionality

--- ArmControl.c
+++ ArmControl.c
@@ -2,43 +2,46 @@
 ARMCONTROL.C
 
 This file contains the code that will control the elbow joint, as
-well as the wrist code. 
+well as the wrist code.
 */
 
 /*
-Activates the two motors at the elbow joint 
-in order to lift the arm. 
+Activates the two motors at the elbow joint
+in order to lift the arm.
 
 Param lift boolean containing whether to raise or drop the elbow
 */
 void liftArm (bool lift){
-  //Check the param being passed 
+  //Check the param being passed
 	nMotorEncoder[motorA]= 0;
+	time1[0] = 0;
 
-	//Why are the two encoder values diffe rent? 
+	//Why are the two encoder values different?
 	if (lift){
 		motor[motorA] = 75;
 		motor[motorC] = 75;
-		while (nMotorEncoder[motorA]<720){};
+		//while (nMotorEncoder[motorA]<720){};
 	}else {
-		//Check encoder value, something wrong 
-		nMotorEncoder[motorA] = 720; 
+		//Check encoder value, something wrong
 		motor[motorA] = -75;
 		motor[motorC] = -75;
-		while (nMotorEncoder[motorA] > 0){}
-		displayString(0, "Down"); 
+		//while (nMotorEncoder[motorA] > 0){}
+		displayString(0, "Down");
+
+    while (time1[0] < 1000){}
+    motor[motorA]=0;
+		motor[motorC]=0;
 	}
-	
-	time1[0] = 0;            
-	while (time1[0] < 2000){} 
-	
+
+	while (time1[0] < 1000){}
+
 	motor[motorA]=0;
 	motor[motorC]=0;
 }
 
 /*
-Rotates the wrist in a given direction through the 
-activation of motors for a specific amount of time. 
+Rotates the wrist in a given direction through the
+activation of motors for a specific amount of time.
 
 Param cw Boolean contains the direction of rotation
 
@@ -47,90 +50,100 @@ Param cw Boolean contains the direction of rotation
 void rotateWrist(bool clockwise){
 
 	//Fix encoder
-	nMotorEncoder[motorB] = 0; 
-	
+	nMotorEncoder[motorB] = 0;
+
 	if (clockwise){
-		motor[motorB] = -25; 
-		while (nMotorEncoder[motorB] >= -90){}	
+		motor[motorB] = -25;
+		while (nMotorEncoder[motorB] >= -90){}
 	}else{
-		motor[motorB] = 25; 
+		motor[motorB] = 25;
 		while (nMotorEncoder[motorB] <= 90){}
 	}
-	
-	time1[0] = 0; 
+
+	time1[0] = 0;
 	while (time1[0] < 2000){}
-	motor[motorB] = 0; 
+	motor[motorB] = 0;
 
 }
 
 void stopArm(){
-	motor[motorA] = 0; 
-  motor[motorC] = 0; 
+	motor[motorA] = 0;
+  motor[motorC] = 0;
 }
 
 
 bool messageAvailable(){
 	if (message == 0){
-		return false; 
+		return false;
 	}else{
-		return true; 
+		return true;
 	}
 }
 
 int getMessage(){
 		if (message != 0){
-			int receivedMessage = message; 
-			ClearMessage(); 
-			return receivedMessage; 
+			int receivedMessage = message;
+			ClearMessage();
+			return receivedMessage;
 		}else{
-			return 0; 
+			return 0;
 
 		}
 }
 
-task main(){ 
-		
-/* 	HOW THEY RECIEVE 
+task main(){
+
+/* 	HOW THEY RECIEVE
  		message_first = messageParm[0];
 	  message_second = messageParm[1];
 	  message_third = messageParm[2];
 */
-  
-	int my_message = 0; 
-	int firstMessage, secondMessage; 
-  
+  ClearMessage();
+
+	int my_message = 0;
+	int firstMessage = 0, secondMessage = 0;
+
 	while (true){
+
 
 		if (messageAvailable() == true){
 
 			firstMessage = messageParm[0];
-      secondMessage = messageParm[1]; 
+      secondMessage = messageParm[1];
 
-      //Action relating to arm 
-			if (firstMessage == 1){
+      if (firstMessage == 1){
         if (secondMessage == 1){
-        	//liftArm(true); 
+        	liftArm(true);
         }else{
           if (secondMessage == 2){
-          	//liftArm(false); 
-          }
-        }
-      }else{
-        
-      //Action relating to wrist 
+            liftArm(false);
+          } //End of if
+        } //End of else
+      } //End of if
+      else{
+
         if (firstMessage == 2){
           if (secondMessage == 1){
-           //rotateWrist(true);
-        	}else {
+          	rotateWrist(true);
+          }else{
             if (secondMessage == 2){
-              //rotateWrist(false);
-          	}
-      		}
-    		}
+            	rotateWrist(false);
+            }
+          }
+        }
+
+        //FOR DEMO ONLY, REMEMBER TO REMOVE
+        if (firstMessage == 3){
+        	displayString(0, "Hello from the");
+        	displayString(1, "phasor domain!!!");
+        }
+
       }
     }
+
+    ClearMessage();
   }
 
-  wait1Msec(100); 
+  wait1Msec(100);
 
 }
--- HandControl.c
+++ HandControl.c
@@ -1,30 +1,30 @@
 /*
-HANDCONTROL.C 
+HANDCONTROL.C
 
 
-This file contains the code that will move the fingers of the hand. 
+This file contains the code that will move the fingers of the hand.
 There are three motors that will be used to move the fingers, with
-the groupings being as follows: 
-	Thumb - MotorA 
+the groupings being as follows:
+	Thumb - MotorA
 	Index and Middle Fingers - MotorB
-	Ring and Pinky Fingers - MotorC 
+	Ring and Pinky Fingers - MotorC
 
 This file will also gather sensor information, which will lead
-to the fingers closing and grasping an object. 
+to the fingers closing and grasping an object.
 */
 
 
 //Moves a general finger
 void moveFinger(int finger, bool close){
-	time1[0] = 0; 
+	time1[0] = 0;
 
 	if (close)
-		motor[finger] = 50; 
+		motor[finger] = 50;
 	else
-		motor[finger] = -50; 
-	
+		motor[finger] = -50;
+
 	while (time1[0] < 2000){}
-	motor[finger] = 0; 
+	motor[finger] = 0;
 }
 
 
@@ -37,24 +37,24 @@ void closeHand(bool close){
 	//Example of closing all the fingers
 	//motorA = 0, motorB = 1, motorC = 2
 	if (close){
-		motor[0] = 50; 
-		motor[1] = 50; 
+		motor[0] = 50;
+		motor[1] = 50;
 	}else{
-		motor[0] = -50; 
-		motor[1] = -50; 
-	} 
-	
-	time1[0] = 0; 
+		motor[0] = -50;
+		motor[1] = -50;
+	}
+
+	time1[0] = 0;
 	while (time1[0] < 2000){}
-	motor[0] = 0; 
-	motor[1] = 0; 
+	motor[0] = 0;
+	motor[1] = 0;
 
 }
 
 void startRoutine(){
   sendMessageWithParm(1, 1); //arm, lift
-  wait1Msec(3000); 
-  
+  wait1Msec(3000);
+
   for (int i = 0; i < 2; i++){
   	sendMessageWithParm(2, 1); //wrist, cw
   	wait1Msec(2000);
@@ -65,12 +65,12 @@ void startRoutine(){
 }
 
 void byeRoutine(){
-  
+
   for (int i = 0; i < 3; i++){
-		closeHand(true); 
-    closeHand(false); 
+		closeHand(true);
+    closeHand(false);
   }
-  
+
   sendMessageWithParm(1, 2); //arm, lift
 
 }
@@ -78,104 +78,124 @@ void byeRoutine(){
 void idObject(int colour, string &object){
 
   if (colour == 1){
-  	object = "Black"; 
+  	object = "Black";
   }else if (colour == 2){
-  	//return "Blue"; 
+  	object = "Pepsi Can";
   }else if (colour == 3){
-  
+
   }else if (colour == 4){
-  	object = "Golf Ball"; 
+  	object = "Golf Ball";
   }else{
-  	object = "White"; 
+  	object = "White";
   }
 
 }
 
 void liftObject () {
-  	
-  	sendMessageWithParm(1, 2);//arm, drop 
-    wait1Msec(2000); 
+
+  	sendMessageWithParm(1, 2);//arm, drop
+    wait1Msec(2000);
  	 	closeHand(true);
-    wait1Msec(2000); 
+    wait1Msec(2000);
   	sendMessageWithParm(1, 1); //arm, lift
-    wait1Msec(2000); 
+    wait1Msec(2000);
   	sendMessageWithParm(2, 1); //wrist, cw
-    wait1Msec(2000); 
+    wait1Msec(2000);
   	sendMessageWithParm(1, 2); //arm, drop
   	wait1Msec(2000);
-  	closeHand(false); 
-  	wait1Msec(2000); 
+  	closeHand(false);
+  	wait1Msec(2000);
   	sendMessageWithParm(1,1);//arm, lift
   	wait1Msec(2000);
   	sendMessageWithParm(2,1);//wrist,cw
   	wait1Msec(2000);
 }
 
+void buttonPressAndRelease(){
+	while (nNxtButtonPressed == -1){}
+	while (nNxtButtonPressed != -1){}
+}
+
+void projectDemo(){
+
+	sendMessageWithParm(3,0); //Bluetooth Message
+	buttonPressAndRelease();
+
+	string object = "";
+	idObject(SensorValue[S2], object);
+	displayString(0, "%s", object);
+	buttonPressAndRelease();
+
+	sendMessageWithParm(1, 1); //arm, lift
+	wait1Msec(3000);
+	sendMessageWithParm(1, 2); //arm, drop
+	wait1Msec(3000);
+	buttonPressAndRelease();
+
+	sendMessageWithParm(2, 1); //wrist, cw
+	wait1Msec(3000);
+	sendMessageWithParm(2, 2); //wrist, cw
+	wait1Msec(3000);
+	buttonPressAndRelease();
+	
+	//ADD DEMO FOR FINGERS
+
+}
+
 
 task main (){
 
-	wait1Msec(500); 
+	wait1Msec(500);
 
 	/*
-	Transmits a sequence of integers ever 1/10th of a second. 
-	NOTE: A value of 0 CANNOT be sent via Bluetooth, as it 
-	indicates that no message is being sent. 
-	*/
-
-	
-	/*
-	sendMessage values: 
-	1 - Function that lifts elbow, rotates wrist, then drops elblow 
-	
+	Transmits a sequence of integers ever 1/10th of a second.
+	NOTE: A value of 0 CANNOT be sent via Bluetooth, as it
+	indicates that no message is being sent.
 	*/
 
 	SensorType[S1] = sensorColorNxtFULL; //Initialize colour sensor
-  int colour = 0; 
-  
+  int colour = 0;
+
   SensorType[S2]=sensorTouch; //Initialize touch sensor
-  bool isTouch = false; 
-  
-  startRoutine(); 
-  
+  bool isTouch = false;
+
+  startRoutine();
+
 	while (true){
 
 
-		while (nNxtButtonPressed == -1){}
-		int button = nNxtButtonPressed; 
-		while (nNxtButtonPressed != -1){}
-    
-    colour = SensorValue[S1]; //Gets colour when button is pressed  
-    
+		while (nNxtButtonPressed == -1 && SensorValue[S2] == 0){}
+		int button = nNxtButtonPressed;
+		int sensorVal = SensorValue[S2];
+		while (nNxtButtonPressed != -1 && SensorValue[S2] != 0){}
+
+		if (sensorVal){
+			displayString(0, "DEMOO");
+			wait1Msec(3000);
+			projectDemo();
+		}
+
+    colour = SensorValue[S1]; //Gets colour when button is pressed
+
     if (button == 3){
-      string object = ""; 
-      idObject(colour, object); 
-      displayString(0, "%s", object); 
-    	liftObject(); 
+      string object = "";
+      idObject(colour, object);
+      displayString(0, "%s", object);
+    	liftObject();
     }else if (button == 2){
-    	//Second routine 
+    	//Second routine
     }else{
-    	//Third routine 
+    	//Third routine
     }
 
-    
+
     /*
-    sendMessageWithParam(part_of_arm, direction_of_motion); 
+    sendMessageWithParam(part_of_arm, direction_of_motion);
     sendMessageWithParm(number_1,number_2,number_3);
-    */ 
-    
-    
-    //Color snesor detects obj --> Via function, pass colour 
-    //Output obj  
-    //Drop hand until touch sensor is activated 
-    //Stop moving 
-    //Close fingers --> How long? Who knows  
-    //Lift object --> Should be same as drop object 
-    //Once at top, rotate wrist
-    //Hangout 
-    //Drop until colour sensor is activated 
-    
-	} //End of while-loop 
-  
-  byeRoutine(); 
-  
+    */
+
+	} //End of while-loop
+
+  byeRoutine();
+
 }
